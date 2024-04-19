3d0e89bd3c53a01bfa5b0ca100e00ba7427b6a2c
Zahra Hanifi <zahrahanifi98@gmail.com>
2024-04-19 21:10:07 +0430

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png" class="w-[500px]">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
